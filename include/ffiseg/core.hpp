0e1b8adac4a223e30a3ad36c35ec2abd949b05d4
hmp-anthony <anthonygoddard962@gmail.com>
2024-03-31 19:06:47 +0100

    added operations

--- include/ffiseg/core.hpp
+++ include/ffiseg/core.hpp
@@ -17,9 +17,19 @@ namespace ffiseg {
             return vector(a*x, a*y, a*z);
         }
 
-        real get_x() { return x; }
-        real get_y() { return y; }
-        real get_z() { return z; }
+        void operator+=(const vector& v) {
+            x += v.get_x();
+            y += v.get_y();
+            z += v.get_z();
+        }
+
+        vector operator+(const vector& v) const {
+            return vector(x + v.get_x(), y + v.get_y(), z + v.get_z()); 
+        }
+
+        real get_x() const { return x; }
+        real get_y() const { return y; }
+        real get_z() const { return z; }
 
         void set_x(real _x) { x = _x; }
         void set_y(real _y) { y = _y; }
@@ -39,6 +49,12 @@ namespace ffiseg {
             return x*x + y*y + z*z;
         }
 
+        void normalize() {
+            real m = magnitude();
+            if(m > 0) {
+                (*this) *= 1.0/m; 
+            }
+        }
     private:
         real x;
         real y;
--- include/ffiseg/tests/core_test.cpp
+++ include/ffiseg/tests/core_test.cpp
@@ -21,6 +21,22 @@ TEST(vector, scalar_multiplication) {
     ASSERT_EQ(w.get_z(), 135.0);
 }
 
+TEST(vector, addition) {
+    ffiseg::vector v1(1.0, 2.0, 3.0);
+    ffiseg::vector v2(3.0, 2.0, 1.0);
+    auto w = v1 + v2;
+
+    ASSERT_EQ(w.get_x(), 4.0);
+    ASSERT_EQ(w.get_y(), 4.0);
+    ASSERT_EQ(w.get_z(), 4.0);
+
+    ffiseg::vector v3(1.0, 1.0, 1.0);
+    v1 += v3;
+    ASSERT_EQ(v1.get_x(), 2.0);
+    ASSERT_EQ(v1.get_y(), 3.0);
+    ASSERT_EQ(v1.get_z(), 4.0);
+}
+
 TEST(vector, invert) {
     ffiseg::vector v(1.0, 2.0, 3.0);
     v.invert();
@@ -38,3 +54,12 @@ TEST(vector, square_magnitude) {
     ffiseg::vector v(1.0, 2.0, 3.0);
     ASSERT_NEAR(v.square_magnitude(), 14.0 , 0.001); 
 }
+
+TEST(vector, normalize) {
+    ffiseg::vector v(1.0, 2.0, 3.0);
+    v.normalize();
+    ASSERT_NEAR(v.get_x(), 0.2673, 0.001); 
+    ASSERT_NEAR(v.get_y(), 0.5345, 0.001); 
+    ASSERT_NEAR(v.get_z(), 0.8018, 0.001); 
+}
+
