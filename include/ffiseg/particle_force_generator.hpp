56861fdae87f26d97ab42fba351adf39368d5cbd
hmp-anthony <anthonygoddard962@gmail.com>
2024-04-06 16:19:19 +0100

    fleshed out pfg

--- include/ffiseg/particle_force_generator.hpp
+++ include/ffiseg/particle_force_generator.hpp
@@ -5,12 +5,45 @@
 #include<vector>
 
 namespace ffiseg {
+    // abstract base class / interface.
     class particle_force_generator {
     public:
         virtual void update_force(particle* part, real duration) = 0;
     };
 
-    // registry keeps track of registrations.
+    class particle_gravity : public particle_force_generator {
+    public:
+        particle_gravity(const vector& gravity);
+        virtual void update_force(particle* part, real duration);
+    private:
+        vector gravity;
+    };
+
+    class particle_drag : public particle_force_generator {
+    public:
+        particle_drag(real k1, real k2);
+        virtual void update_force(particle* part, real duration);
+    private:
+        real k1;
+        real k2;
+    };
+   
+    // generator that applies a spring force where one end is 
+    // attached to a fixed point in space.
+    class particle_anchored_spring : public particle_force_generator {
+    public:
+        particle_anchored_spring();
+        particle_anchored_spring(vector* anchor, real spring_constant, real rest_length);
+        const vector* get_anchor() const;
+        void init(vector* anchor, real spring_constant, real rest_length);
+        virtual void update_force(particle* part, real duration);
+    protected:
+        vector* anchor;
+        real spring_constant;
+        real rest_length;
+    };
+
+    // registry keeps track of force registrations.
     class particle_force_registry {
     public:
         void add(particle* part, particle_force_generator* fg);
--- src/particle_force_generator.cpp
+++ src/particle_force_generator.cpp
@@ -2,6 +2,61 @@
 
 using namespace ffiseg;
 
+particle_gravity::particle_gravity(const vector& gravity) : gravity(gravity) {}
+
+void particle_gravity::update_force(particle* part, real duration) {
+    if (!part->has_finite_mass()) return;
+    part->add_force(gravity * part->get_mass());
+}
+
+particle_drag::particle_drag(real k1, real k2) : k1(k1), k2(k2) {}
+
+void particle_drag::update_force(particle* part, real duration) {
+    vector force;
+    part->get_velocity(&force);
+
+    // calculate the total drag coefficient
+    real drag_coeff = force.magnitude();
+    drag_coeff = k1 * drag_coeff + k2 * drag_coeff * drag_coeff;
+
+    // calculate the final force and apply it - see page 100
+    force.normalize();
+    force *= -drag_coeff;
+    part->add_force(force);
+}
+
+particle_anchored_spring::particle_anchored_spring() {}
+
+particle_anchored_spring::particle_anchored_spring(vector *anchor, real sc, real rl) 
+    : anchor(anchor), spring_constant(sc), rest_length(rl) {}
+
+const vector* particle_anchored_spring::get_anchor() const { 
+    return anchor;
+}
+
+void particle_anchored_spring::init(vector* anchor, real sc, real rl) {
+    particle_anchored_spring::anchor = anchor;
+    particle_anchored_spring::spring_constant = sc;
+    particle_anchored_spring::rest_length = rl;
+}
+
+void particle_anchored_spring::update_force(particle* part, real duration)
+{
+    // Calculate the vector of the spring
+    vector force;
+    part->get_position(&force);
+    force -= *anchor;
+
+    // Calculate the magnitude of the force
+    real magnitude = force.magnitude();
+    magnitude = (rest_length - magnitude) * spring_constant;
+
+    // Calculate the final force and apply it
+    force.normalize();
+    force *= magnitude;
+    part->add_force(force);
+}
+
 void particle_force_registry::add(particle* part, 
                                   particle_force_generator* force_gen) {
     particle_force_registry::particle_force_registration reg;
