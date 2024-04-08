14add5d080a261c46ca231702b11fd1272f495fe
hmp-anthony <anthonygoddard962@gmail.com>
2024-04-08 12:23:04 +0100

    added test for particle_anchored_spring

--- tests/particle_force_generator_test.cpp
+++ tests/particle_force_generator_test.cpp
@@ -28,3 +28,21 @@ TEST(particle_force_registry, particle_spring) {
     ASSERT_EQ(reg_q.part->get_position().get_y(), -11);
 }
 
+TEST(particle_force_registry, particle_anchored_spring) {
+    ffiseg::particle_force_registry pfr;
+    ffiseg::vector p(0, 0, 0);
+    ffiseg::particle q(0, 5.0f, 0);
+    q.set_mass(1.0f);
+
+    ffiseg::particle_anchored_spring ps_p(&p, 4.0, 1.0);
+    pfr.add(&q, &ps_p);
+   
+    pfr.update_forces(1.0f);
+
+    auto reg_q = pfr.get_registrations()[0];
+    reg_q.part->integrate(1.0f);
+    reg_q.part->integrate(1.0f);
+
+    ASSERT_EQ(reg_q.part->get_mass(), 1);
+    ASSERT_EQ(reg_q.part->get_position().get_y(), -11);
+}
