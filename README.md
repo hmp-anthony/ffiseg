7cfc3ba43d7f0d02b9ee49626c8fa70d4eeb2c7c
hmp-anthony <anthonygoddard962@gmail.com>
2024-04-09 19:02:40 +0100

    updated readme and fleshed out particle_contact

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 A physics engine based on the Cyclone physics engine.
 
-I have improved the code and added tests to increase robustness. 
+I have improved the code and added tests to increase robustness.
+
+No more make files. CMake is used as the build system.
+
+Just type `make` and then `./build/demos/ballistic/ballistic`, for instance.
 
 I hope to add to the code base by adding:
 
--- src/particle_contact.cpp
+++ src/particle_contact.cpp
@@ -2,6 +2,7 @@
 
 void particle_contact::resolve(real duration) {
     resolve_velocity(duration);
+    resolve_interpenetration(duration);
 }
 
 real particle_contact::calculate_separating_velocity() const {
@@ -35,6 +36,7 @@ void particle_contact::resolve_velocity(real duration) {
     vector impulse_per_inverse_mass = contact_normal * impulse;
 
     // apply impulses;
+    // page 131; p' = p + g/m; g - impulse
     parts[0]->set_velocity(parts[0]->get_velocity()
     + impulse_per_inverse_mass * parts[0]->get_inverse_mass());
     
@@ -43,3 +45,24 @@ void particle_contact::resolve_velocity(real duration) {
         + impulse_per_inverse_mass * -parts[1]->get_inverse_mass());
     }
 }
+
+void particle_contact::resolve_interpenetration(real duration) {
+    if(penetration <= 0) return;
+    
+    real total_inverse_mass = parts[0]->get_inverse_mass();
+    if(parts[1]) {
+        total_inverse_mass += parts[1]->get_inverse_mass();
+    }
+
+    if(total_inverse_mass <= 0) return;
+   
+    vector move_per_inverse_mass = contact_normal * (-penetration / total_inverse_mass);
+
+    parts[0]->set_position(parts[0]->get_position()
+    + move_per_inverse_mass * parts[0]->get_inverse_mass());
+    
+    if(parts[1]) {
+        parts[1]->set_position(parts[1]->get_position()
+        + move_per_inverse_mass * -parts[1]->get_inverse_mass());
+    }
+}   
