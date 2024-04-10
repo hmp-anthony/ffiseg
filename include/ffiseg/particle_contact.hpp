53586a49db959a907ed3a5fb8f88229ec977c548
hmp-anthony <anthonygoddard962@gmail.com>
2024-04-10 17:16:18 +0100

    added particle_contact_resolver

--- include/ffiseg/particle_contact.hpp
+++ include/ffiseg/particle_contact.hpp
@@ -3,12 +3,14 @@
 
 #include<ffiseg/particle.hpp>
 
+#include<vector>
+
 namespace cyclone {
 
-    //class particle_contact_resolver;
+    class particle_contact_resolver;
 
     class particle_contact {
-        //friend class particle_contact_resolver;
+        friend class particle_contact_resolver;
     protected:
         void resolve(real duration);
         real calculate_separating_velocity() const;
@@ -23,6 +25,17 @@ namespace cyclone {
         void resolve_interpenetration(real duration);
     };
 
+    class particle_contact_resolver {
+    public:
+        particle_contact_resolver(unsigned inters);
+        void set_iterations(unsigned iters);
+        void resolve_contacts(particle_contact* contact_array,
+                              unsigned contact_array_size,
+                              real duration);
+    protected:
+        unsigned iterations;
+        unsigned iterations_used;
+    };
 }
 
 #endif
--- src/particle_contact.cpp
+++ src/particle_contact.cpp
@@ -23,6 +23,22 @@ void particle_contact::resolve_velocity(real duration) {
     }
     
     real new_sv = -sv * restitution;
+
+    // check the velocity build-up due to acceleration only.
+    vector acc_caused_velocity = parts[0]->get_acceleration();
+    if(part[1]) {
+        acc_caused_velocity -= parts[1]->get_acceleration();
+    }
+
+    real acc_caused_sep_velocity = acc_caused_velocity * contact_normal * duration;
+    
+    // if we have got a closing veclocity due to acceleration build up
+    // remove it from the sep vel.
+    if(acc_caused_sep_velocity < 0) {
+        new_sv += restitution * acc_caused_sep_velocity;
+        if(new_sv < 0) new_sv = 0;
+    }
+
     real delta_velocity = new_sv - sv;
     
     // objects with larger mass get less change in velocity.
@@ -56,13 +72,74 @@ void particle_contact::resolve_interpenetration(real duration) {
 
     if(total_inverse_mass <= 0) return;
    
-    vector move_per_inverse_mass = contact_normal * (-penetration / total_inverse_mass);
+    vector move_per_inverse_mass = contact_normal * (penetration / total_inverse_mass);
 
-    parts[0]->set_position(parts[0]->get_position()
-    + move_per_inverse_mass * parts[0]->get_inverse_mass());
-    
+    particle_movement[0] = move_per_inverse_mass * parts[0]->get_inverse_mass();
     if(parts[1]) {
-        parts[1]->set_position(parts[1]->get_position()
-        + move_per_inverse_mass * -parts[1]->get_inverse_mass());
+        particle_movement[1] = move_per_inverse_mass * -parts[1]->get_inverse_mass();
+    } else {
+        particle_movement[1].clear();
     }
-}   
+
+    parts[0]->set_position(parts[0]->get_position() + particle_movement[0]);
+    if(parts[1]) {
+        parts[1]->set_position(parts[1]->get_position() + particle_movement[1]);
+    }
+}
+
+particle_contact_resolver::particle_contact_resolver(unsigned iters) : iterations(iters) {}
+
+void particle_contact_resolver::set_iterations(unsigned iters) {
+    iterations = iters;
+}
+
+void particle_contact_resolver::resolve_contacts(particle_contact* contact_array,
+                                                 unsigned contact_array_size,
+                                                 real duration) {
+    unsigned i = 0;
+    iterations_used = 0;
+
+    while(iterations_used < iterations) {
+        real max = REAL_MAX;
+        unsigned max_index = contact_array_size;
+
+        for(i = 0; i < contact_array_size; ++i) {
+            real sv = contact_array[i].calculate_separating_velocity();
+            if(sv < max && (sv < 0 || contact_array[i].penetration > 0)) {
+                max = sv;
+                max_index = i;
+            }
+        }
+        
+        if(max_index == contact_array_size) break;
+
+        contact_array[max_index].resolve(duration);
+        vector* move = contact_array[max_index].particle_movement;
+
+        for (i = 0; i < contact_array_size; i++) {
+            if (contact_array[i].parts[0] == contact_array[max_index].parts[0])
+            {
+                contact_array[i].penetration -= move[0] * contact_array[i].contact_normal;
+            }
+
+            // isnt this a bug?! BUG
+            else if (contact_array[i].parts[0] == contact_array[max_index].parts[1])
+            {
+                contact_array[i].penetration -= move[1] * contact_array[i].contact_normal;
+            }
+            if (contact_array[i].parts[1])
+            {
+                if (contact_array[i].parts[1] == contact_array[max_index].parts[0])
+                {
+                    contact_array[i].penetration += move[0] * contact_array[i].contact_normal;
+                }
+                else if (contact_array[i].parts[1] == contact_array[max_index].parts[1])
+                {
+                    contact_array[i].penetration += move[1] * contact_array[i].contact_normal;
+                }
+            }
+        }
+
+        iterations_used++
+    }
+}
